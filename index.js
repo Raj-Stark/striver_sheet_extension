d3f103c54a195acce8b7d6e39c4c2fdd51b2f06c
Raj Stark <rpal778866@gmail.com>
2023-10-31 08:09:52 +0530

    Chrome lamda changes

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const cors = require("cors");
+const Chromium = require("chrome-aws-lambda");
 
 const app = express();
 const PORT = process.env.PORT | 3000;
 app.use(cors());
 
+if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+}else{
+  puppeteer = require("puppeteer");
+}
+
 app.get("/scrape", async (req, res) => {
+ let options = {};
+  
+if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
+  options = {
+    args:[...chrome.args],
+    defaultViewport:chrome.defaultViewport,
+    executablePath:await chrome.executablePath,
+    headless:true,
+    ignoreHTTPSErrors:true,
+  }
+ 
+}
+
+
   const searchQuery = req.query.q;
 
   if (!searchQuery) {
     return res.status(400).json({ error: "Missing 'q' parameter in the URL." });
   }
 
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
 
   try {
